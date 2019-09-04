8d71ecfa9511a04abc4fb67b637b9bbf0422269a
Tnze <cjd001113@outlook.com>
2019-09-04 09:08:05 +0800

    更新新版SDK

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 :: 关闭控制台回显  
 @echo off
 
+:: 生成app.json
+go build github.com/Tnze/CoolQ-Golang-SDK/tools/cqcfg
+go generate
+
 :: 设置环境变量  
 SET CGO_LDFLAGS=-Wl,--kill-at
 SET CGO_ENABLED=1
 SET GOOS=windows
 SET GOARCH=386
 SET GOPROXY=https://goproxy.cn
-
-:: 生成app.json
-go build github.com/Tnze/CoolQ-Golang-SDK/tools/cqcfg
-go generate
 
 :: 编译app.dll  
 go build -buildmode=c-shared -o app.dll
