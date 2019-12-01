b703eed409671024755d364f7f8b41f0ccc12954
Tnze <cjd001113@outlook.com>
2019-12-01 11:49:46 +0800

    更新编译指令

--- build.bat
+++ build.bat
@@ -12,10 +12,10 @@ SET GOOS=windows
 SET GOARCH=386
 SET GOPROXY=https://goproxy.cn
 
-:: 编译app.dll  
-go build -buildmode=c-shared -o app.dll
+:: 编译app.dll
+go build -ldflags "-s -w" -buildmode=c-shared -o app.dll
 
 :: 如果设置了环境变量，则把app.dll和app.json复制到酷Q的dev文件夹
-REM SET DevDir=D:\酷Q Pro\dev\cn.miaoscraft.sis
+R:: SET DevDir=D:\酷Q Pro\dev\cn.miaoscraft.sis
 if defined DevDir
 for %%f in (app.dll,app.json) do move %%f "%DevDir%\%%f" > nul
