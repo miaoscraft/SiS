4453b4471b85404fd9307fc446f81aa03b9b8e7c
Tnze <cjd001113@outlook.com>
2020-03-31 04:50:16 +0800

    fix当ret==nil时RCON执行出错的问题

--- data/rcon.go
+++ data/rcon.go
@@ -17,40 +17,43 @@ func openRCON(address, password string) error {
 	return nil
 }
 
-// RCONCmd 执行RCON命令，断线时尝试重连一次
+// RCONCmd 执行RCON命令，每次都创建一个连接。
+// 当ret不为nil时，通过回调方式返回RCON执行结果，ret可能被调用多次。
 func RCONCmd(cmd string, ret func(string)) error {
 	var r *mcnet.RCONConn
-	for {
-		if rconDialer == nil {
-			return errors.New("RCON未设置")
-		}
-		conn, err := rconDialer()
-		if err != nil {
-			return err
-		}
-		r = conn.(*mcnet.RCONConn)
 
-		err = r.Cmd(cmd)
-		if err == nil {
-			break
-		}
-		Logger.Errorf("RCON执行失败: %v", err)
+	if rconDialer == nil {
+		return errors.New("RCON未设置")
 	}
+	conn, err := rconDialer()
+	if err != nil {
+		return err
+	}
+	r = conn.(*mcnet.RCONConn)
+
+	err = r.Cmd(cmd)
+	if err != nil {
+		return err
+	}
+
 	go func() {
 		defer r.Close()
+		if ret == nil {
+			return
+		}
 		tip := time.AfterFunc(time.Second, func() {
 			ret("正在努力发送指令噢，请稍后~")
 		})
-		for ret != nil {
+		for {
 			_ = r.SetDeadline(time.Now().Add(time.Second * 10))
 			resp, err := r.Resp()
 			if err != nil {
-				Logger.Infof("停止转发RCON返回值: %v", err)
+				Logger.Debugf("停止转发RCON返回值: %v", err)
 				return
 			}
 			tip.Stop() // 不再发送提示
 
-			Logger.Infof("RCON返回: %q", resp)
+			Logger.Debugf("RCON返回: %q", resp)
 			// 过滤掉末尾换行符、空格和零字符，过滤§格式字符串
 			resp = chat.Message{Text: strings.TrimRight(resp, " \000\n")}.ClearString()
 			ret(resp)
